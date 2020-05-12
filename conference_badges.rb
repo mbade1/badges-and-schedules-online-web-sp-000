60491594ae516d5158d53a5989d1c2aca48caa9f
Michael Bade <mbade314@gmail.com>
2020-05-12 00:59:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  var = attendees.push(badge_maker(name))
-  return var
+  array = []
+  array.push(badge_maker(name))
+  return array
 end
