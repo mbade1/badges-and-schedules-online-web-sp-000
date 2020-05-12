4d8ac0ff7e0bf3e53c183d84f6bcc5ce0de35a16
Michael Bade <mbade314@gmail.com>
2020-05-12 01:15:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,13 +28,3 @@ def printer(attendees)
     puts badg
   end
 end
-#
-# def printer(attendees)
-#   batch_badge_creator(attendees).each do |badge|
-#     puts badge
-#   end
-#
-#   assign_rooms(attendees).each do |badge|
-#     puts badge
-#   end
-# end
